ba042028edf48e1a90e6f9990a1c6799aa54b74e
kotov anton <fynjybq_njyz5@mail.ru>
2024-04-21 20:53:39 +0300

    Удалил воспроизведение аудио через сторонний плеер

--- audio_control.py
+++ audio_control.py
@@ -2,8 +2,7 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from config import STATIC
-from pydub import AudioSegment
-from pydub.playback import play
+
 
 
 # Функция для увеличения громкости на 5%
@@ -24,9 +23,7 @@ def decrease_volume():
     current_volume = volume.GetMasterVolumeLevelScalar()
     volume.SetMasterVolumeLevelScalar(max(0.0, current_volume - 0.05), None)
 
-def play_audio(file_path):
-    audio = AudioSegment.from_file(file_path, format="m4a")
-    play(audio)
+
 
 # Пример использования
 #audio_file = "D:\\projects\\pythonProjects\\lazy_boy_bot\\static\\eto_vtoroy.m4a"
