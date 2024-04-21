95466b641f3087c428d92c8a472a8e1190d19222
anton <aiker03@mail.ru>
2024-04-21 21:05:51 +0300

    удалил congif

--- config.py
+++
@@ -1,8 +0,0 @@
-# Устанавливаем токен бота
-API_TOKEN = '6663287324:AAFtuJpKDvN-MNac5wam7ysoGTAzQntBG2E'
-
-# ID админа и Димаса (замени на реальные значения)
-ADMIN_ID = 480710961
-DIMAS_ID = -1
-
-STATIC = "static/"
